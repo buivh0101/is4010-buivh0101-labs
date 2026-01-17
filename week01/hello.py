8abae2f73922d83b2a4e53dee4a503918d3dae83
Anh Bui <buivh@mail.uc.edu>
2026-01-17 13:56:18 -0500

    Complete Lab 01: add hello.py

---
+++ week01/hello.py
@@ -0,0 +1,16 @@
+"""
+Lab 01: Development Toolkit Setup
+Author: Anh Bui
+"""
+
+def main():
+    print("Hello, IS4010!")
+    print("My development environment is ready.")
+
+    import sys
+    print(f"Python version: {sys.version}")
+    print(f"Python executable: {sys.executable}")
+
+if __name__ == "__main__":
+    main()
+
